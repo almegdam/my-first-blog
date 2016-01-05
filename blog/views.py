b9f1a3f1134fa2e0c12e0a52240440900dfebbea
ahmed <almegdam2013@gmail.com>
2016-01-05 19:52:12 +0300

    edit version 1.2

--- blog/views.py
+++ blog/views.py
@@ -39,12 +39,12 @@ def post_edit(request, pk):
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm(instance=post)
- 	return render(request, 'blog/post_edit.html', {'form': form})
+	return render(request, 'blog/post_edit.html', {'form': form})
 
 @login_required
 def post_draft_list(request):
-    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
-    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+	return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 @login_required
 def post_publish(request, pk):
